9a6f88c3648ccc41c71e9fd6161bca36cef38114
Sarah Gerweck <sarah.a180@gmail.com>
2013-06-08 18:10:12 -0700

    Remove deprecated accessor.
    Initially, `getLogger` was accessible from the `Logger` object, but now
    it is available directly from the `org.log4s` package, which makes it
    much more convenient to simply `import org.log4s._` to get all the
    imports and functionality you might need.

--- src/main/scala/org/log4s/Logger.scala
+++ src/main/scala/org/log4s/Logger.scala
@@ -14,9 +14,6 @@ object Logger {
   final val singletonsByName = true
   final val trailingDollar = false
   
-  @deprecated("0.1", "Use org.log4s.getLogger")
-  def getLogger: Logger = macro LoggerMacros.getLoggerImpl
-  
   sealed trait LevelLogger extends Any {
     def isEnabled: Boolean
     def apply(msg: => String): Unit
