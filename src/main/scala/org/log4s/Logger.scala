128a5e9651f9e85ba52c804f6ae4e118ad009471
Sarah Gerweck <sarah.a180@gmail.com>
2014-06-01 20:43:13 -0700

    Minor cleanup of (some) Scala 2.11 warnings.
    Unfortunately, I can't make the macros work on 2.10 and warning-free on
    2.11, so for now we'll live with a few warnings. I'll have to either
    retire 2.10 support or have two macro providers when Scala 2.12 drops.

--- src/main/scala/org/log4s/Logger.scala
+++ src/main/scala/org/log4s/Logger.scala
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory.{ getLogger => getJLogger }
 object Logger {
   final val singletonsByName = true
   final val trailingDollar = false
-  
+
   sealed trait LevelLogger extends Any {
     def isEnabled: Boolean
     def apply(msg: => String): Unit
@@ -25,25 +25,25 @@ object Logger {
     @inline def apply(msg: => String) = if (isEnabled) logger.trace(msg)
     @inline def apply(t: Throwable)(msg: => String) = if (isEnabled) logger.trace(msg, t)
   }
-  
+
   final class DebugLevelLogger private[log4s](val logger: JLogger) extends AnyVal with LevelLogger {
     @inline def isEnabled = logger.isDebugEnabled
     @inline def apply(msg: => String) = if (isEnabled) logger.debug(msg)
     @inline def apply(t: Throwable)(msg: => String) = if (isEnabled) logger.debug(msg, t)
   }
-  
+
   final class InfoLevelLogger private[log4s](val logger: JLogger) extends AnyVal with LevelLogger {
     @inline def isEnabled = logger.isInfoEnabled
     @inline def apply(msg: => String) = if (isEnabled) logger.info(msg)
     @inline def apply(t: Throwable)(msg: => String) = if (isEnabled) logger.info(msg, t)
   }
-  
+
   final class WarnLevelLogger private[log4s](val logger: JLogger) extends AnyVal with LevelLogger {
     @inline def isEnabled = logger.isWarnEnabled
     @inline def apply(msg: => String) = if (isEnabled) logger.warn(msg)
     @inline def apply(t: Throwable)(msg: => String) = if (isEnabled) logger.warn(msg, t)
   }
-  
+
   final class ErrorLevelLogger private[log4s](val logger: JLogger) extends AnyVal with LevelLogger {
     @inline def isEnabled = logger.isErrorEnabled
     @inline def apply(msg: => String) = if (isEnabled) logger.error(msg)
@@ -53,25 +53,25 @@ object Logger {
 
 final class Logger(val logger: JLogger) extends AnyVal {
   @inline def isTraceEnabled: Boolean = logger.isTraceEnabled
-  
+
   @inline def isDebugEnabled: Boolean = logger.isDebugEnabled
-  
+
   @inline def isInfoEnabled: Boolean = logger.isInfoEnabled
-  
+
   @inline def isWarnEnabled: Boolean = logger.isWarnEnabled
-  
+
   @inline def isErrorEnabled: Boolean = logger.isErrorEnabled
-  
-  
+
+
   import Logger._
-  
+
   /* These will allow maximum inlining if the type is known at compile time. */
   @inline def apply(lvl: Trace.type): TraceLevelLogger = new TraceLevelLogger(logger)
   @inline def apply(lvl: Debug.type): DebugLevelLogger = new DebugLevelLogger(logger)
   @inline def apply(lvl: Info .type): InfoLevelLogger  = new InfoLevelLogger (logger)
   @inline def apply(lvl: Warn .type): WarnLevelLogger  = new WarnLevelLogger (logger)
   @inline def apply(lvl: Error.type): ErrorLevelLogger = new ErrorLevelLogger(logger)
-  
+
   def apply(level: LogLevel): LevelLogger = level match {
     case Trace => new TraceLevelLogger(logger)
     case Debug => new DebugLevelLogger(logger)
@@ -79,30 +79,30 @@ final class Logger(val logger: JLogger) extends AnyVal {
     case Warn  => new WarnLevelLogger(logger)
     case Error => new ErrorLevelLogger(logger)
   }
-  
+
   import LoggerMacros._
-  
-  def trace(t: Throwable)(msg: String) = macro traceTM
-  def trace(msg: String) = macro traceM
-  
-  def debug(t: Throwable)(msg: String) = macro debugTM
-  def debug(msg: String) = macro debugM
-  
-  def info(t: Throwable)(msg: String) = macro infoTM
-  def info(msg: String) = macro infoM
-  
-  def warn(t: Throwable)(msg: String) = macro warnTM
-  def warn(msg: String) = macro warnM
-  
-  def error(t: Throwable)(msg: String) = macro errorTM
-  def error(msg: String) = macro errorM
-  
+
+  def trace(t: Throwable)(msg: String): Unit = macro traceTM
+  def trace(msg: String): Unit = macro traceM
+
+  def debug(t: Throwable)(msg: String): Unit = macro debugTM
+  def debug(msg: String): Unit = macro debugM
+
+  def info(t: Throwable)(msg: String): Unit = macro infoTM
+  def info(msg: String): Unit = macro infoM
+
+  def warn(t: Throwable)(msg: String): Unit = macro warnTM
+  def warn(msg: String): Unit = macro warnM
+
+  def error(t: Throwable)(msg: String): Unit = macro errorTM
+  def error(msg: String): Unit = macro errorM
+
 }
 
 private object LoggerMacros {
   final def getLoggerImpl(c: Context): c.Expr[Logger] = {
     import c.universe._
-    
+
     val cls = c.enclosingClass.symbol
 
     if (Logger.singletonsByName) {
@@ -111,21 +111,21 @@ private object LoggerMacros {
         return reify { new Logger(getJLogger(name.splice)) }
       }
     }
-    
+
     assert(cls.isModule || cls.isClass, "Enclosing class is always either a module or a class")
-    
+
     val tp = if (cls.isModule) cls.asModule.moduleClass else cls
-    
+
     val expr = c.Expr[Class[_]](Literal(Constant(tp.asType.toTypeConstructor)))
     reify { new Logger(getJLogger(expr.splice)) }
   }
-  
-  
+
+
   private[this] type LogCtx = Context { type PrefixType = Logger }
-  
+
   @inline private[this] def reflectiveLog(c: LogCtx)(msg: c.Expr[String], error: Option[c.Expr[Throwable]])(logLevel: LogLevel) = {
     import c.universe._
-        
+
     val logger = Select(c.prefix.tree, newTermName("logger"))
     val logValues = error match {
       case None    => List(msg.tree)
@@ -133,27 +133,27 @@ private object LoggerMacros {
     }
     val logExpr = c.Expr[Unit](Apply(Select(logger, newTermName(logLevel.methodName)), logValues))
     def checkExpr = c.Expr[Boolean](Apply(Select(logger, newTermName(s"is${logLevel.name}Enabled")), Nil))
-    
+
     msg match {
       case c.Expr(Literal(Constant(_))) => logExpr
       case _ =>
-        reify { if (checkExpr.splice) logExpr.splice }  
+        reify { if (checkExpr.splice) logExpr.splice }
     }
   }
-  
+
   def traceTM(c: LogCtx)(t: c.Expr[Throwable])(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, Some(t))(Trace)
   def traceM(c: LogCtx)(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, None)(Trace)
-  
+
   def debugTM(c: LogCtx)(t: c.Expr[Throwable])(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, Some(t))(Debug)
   def debugM(c: LogCtx)(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, None)(Debug)
-  
+
   def infoTM(c: LogCtx)(t: c.Expr[Throwable])(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, Some(t))(Info)
   def infoM(c: LogCtx)(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, None)(Info)
-  
+
   def warnTM(c: LogCtx)(t: c.Expr[Throwable])(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, Some(t))(Warn)
   def warnM(c: LogCtx)(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, None)(Warn)
-  
+
   def errorTM(c: LogCtx)(t: c.Expr[Throwable])(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, Some(t))(Error)
   def errorM(c: LogCtx)(msg: c.Expr[String]): c.Expr[Unit] = reflectiveLog(c)(msg, None)(Error)
-  
+
 }
--- src/main/scala/org/log4s/package.scala
+++ src/main/scala/org/log4s/package.scala
@@ -3,7 +3,7 @@ package org
 import language.experimental.macros
 
 package object log4s {
-  def getLogger = macro LoggerMacros.getLoggerImpl
+  def getLogger: org.log4s.Logger = macro LoggerMacros.getLoggerImpl
   def getLogger(name: String) = new Logger(org.slf4j.LoggerFactory.getLogger(name))
   def getLogger(clazz: Class[_]) = new Logger(org.slf4j.LoggerFactory.getLogger(clazz))
 }
