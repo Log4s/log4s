f804e6347f46edd14a028be852bc70088730185b
Sarah Gerweck <sarah.a180@gmail.com>
2014-07-29 21:02:58 -0700

    Automatically do signed releases.

--- project/Build.scala
+++ project/Build.scala
@@ -124,7 +124,33 @@ object PublishSettings {
 }
 
 object Release {
-  val settings = releaseSettings
+  import sbtrelease._
+  import ReleaseStateTransformations._
+  import ReleasePlugin._
+  import ReleaseKeys._
+  import Utilities._
+  import com.typesafe.sbt.SbtPgp.PgpKeys._
+
+  val settings = releaseSettings ++ Seq (
+    ReleaseKeys.releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      publishArtifacts.copy(action = publishSignedAction),
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    )
+  )
+
+  lazy val publishSignedAction = { st: State =>
+    val extracted = st.extract
+    val ref = extracted.get(thisProjectRef)
+    extracted.runAggregated(publishSigned in Global in ref, st)
+  }
 }
 
 object Eclipse {
