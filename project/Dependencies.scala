a537ea59167253074767321e58cdf04b6bdc9284
Sarah Gerweck <sarah.a180@gmail.com>
2017-06-01 21:47:49 -0700

    Update versions of dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 import sbt._
 
 object Dependencies {
-  final val slf4jVersion     = "1.7.21"
-  final val logbackVersion   = "1.1.7"
-  final val scalatestVersion = "3.0.1"
+  final val slf4jVersion     = "1.7.25"
+  final val logbackVersion   = "1.2.3"
+  final val scalatestVersion = "3.0.3"
 
   val slf4j     = "org.slf4j"      %  "slf4j-api"       % slf4jVersion
   val logback   = "ch.qos.logback" %  "logback-classic" % logbackVersion
