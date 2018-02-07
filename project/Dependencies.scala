5af701efb962ab5e054a4c7434056101b49f9c5c
Sarah Gerweck <sarah.a180@gmail.com>
2018-02-07 12:03:19 -0800

    Update Scala 2.13 to milestone 2
    Also, update Scalatest to 3.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   final val slf4jVersion     = "1.7.25"
   final val logbackVersion   = "1.2.3"
-  final val scalatestVersion = "3.0.3"
+  final val scalatestVersion = "3.0.5"
 
   val slf4j     = "org.slf4j"      %  "slf4j-api"       % slf4jVersion
   val logback   = "ch.qos.logback" %  "logback-classic" % logbackVersion
