b3f1c8e895a43bde9005913467bb25170fe22e5a
Sarah Gerweck <sarah.a180@gmail.com>
2018-03-16 02:14:11 -0700

    Expose All and Off thresholds in `log4s` package

--- core/js/src/main/scala/org/log4s/package-platform.scala
+++ core/js/src/main/scala/org/log4s/package-platform.scala
@@ -2,4 +2,6 @@ package org.log4s
 
 trait `package-platform` {
   final val Log4sConfig: log4sjs.Log4sConfig = log4sjs.Log4sConfig
+  final val AllThreshold = log4sjs.AllThreshold
+  final val OffThreshold = log4sjs.OffThreshold
 }
