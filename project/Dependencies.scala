d83023aaa619cd891e25a2836a818f5af782ded1
Ross A. Baker <ross@rossabaker.com>
2019-06-10 12:58:53 -0400

    Support Scala 2.13, drop 2.13.0-RC*

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,15 +5,7 @@ object Dependencies {
   final val slf4jVersion      = "1.7.25"
   final val logbackVersion    = "1.2.3"
   final val scalacheckVersion = "1.14.0"
-
-  val scalatestVersion = Def.map(scalaBinaryVersion) {
-    case "2.13.0-M3"  => "3.0.5-M1"
-    case "2.13.0-M5"  => "3.0.6-SNAP6"
-    case "2.13.0-RC1" => "3.0.8-RC2"
-    case "2.13.0-RC2" => "3.0.8-RC4"
-    case "2.13.0-RC3" => "3.0.8-RC5"
-    case other        => "3.0.5"
-  }
+  final val scalatestVersion  = "3.0.8"  
 
   val slf4j     = "org.slf4j"      % "slf4j-api"       % slf4jVersion
   val logback   = "ch.qos.logback" % "logback-classic" % logbackVersion
