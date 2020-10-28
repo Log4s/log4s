c9f6b2bd2eb4ee86bdaf993fe7772421d6350558
sullis <github@seansullivan.com>
2020-10-28 08:39:28 -0700

    Scala 2.13.3 (#50)
    Co-authored-by: Ross A. Baker <ross@rossabaker.com>

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -16,7 +16,7 @@ trait ProjectSettings
   override final val githubProject            = "log4s"
 
   override final val buildScalaVersion        = "2.12.11"
-  override final val extraScalaVersions       = Seq("2.11.12", "2.13.2")
+  override final val extraScalaVersions       = Seq("2.11.12", "2.13.3")
   override final val minimumJavaVersion       = "1.7"
   override final val defaultOptimize          = true
   override final val defaultOptimizeGlobal    = true
