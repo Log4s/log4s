83ff8021183834a62f00ed5cb5ca662eade4ce57
Seth Tisue <seth@tisue.net>
2020-11-09 20:06:22 -0800

    upgrade to ScalaCheck 1.15 and ScalaTest 3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,9 @@ import Keys._
 object Dependencies {
   final val slf4jVersion                   = "1.7.25"
   final val logbackVersion                 = "1.2.3"
-  final val scalacheckVersion              = "1.14.3"
-  final val scalatestVersion               = "3.1.1"
-  final val scalatestPlusScalacheckVersion = "3.1.1.1"
+  final val scalacheckVersion              = "1.15.1"
+  final val scalatestVersion               = "3.2.3"
+  final val scalatestPlusScalacheckVersion = "3.2.2.0"
   final val scalajsStubsVersion            = "1.0.0"
 
   val slf4j     = "org.slf4j"      % "slf4j-api"       % slf4jVersion
