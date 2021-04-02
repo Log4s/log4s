b99144a8906d6e32506b3e11535bf255a4a93e31
Ross A. Baker <ross@rossabaker.com>
2021-04-01 21:54:28 -0400

    Scala 2.12.13 and 2.13.5

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -15,8 +15,8 @@ trait ProjectSettings
   override final val githubOrganization       = "Log4s"
   override final val githubProject            = "log4s"
 
-  override final val buildScalaVersion        = "2.12.12"
-  override final val extraScalaVersions       = Seq("2.11.12", "2.13.4", "3.0.0-RC1", "3.0.0-RC2")
+  override final val buildScalaVersion        = "2.12.13"
+  override final val extraScalaVersions       = Seq("2.11.12", "2.13.5", "3.0.0-RC1", "3.0.0-RC2")
   override final val minimumJavaVersion       = "1.7"
   override final val defaultOptimize          = true
   override final val defaultOptimizeGlobal    = true
